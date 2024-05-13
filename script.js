34dddff9a5215e7cb1ec788897af1a4d8f0ee54c
yogesh <yogesh.yogesh.mec20@iitbhu.ac.in>
2024-05-13 10:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function daysOfAYear(n)
+{
+	if((n%400==0)||((n%100!=0)&&(n%4==0)))
+	{
+		return 366;
+	}
+	return 365;
+	
+}
+let value1 = prompt("enter the year.");
+let a=daysOfAYear(value1);
+ alert(a);
+
